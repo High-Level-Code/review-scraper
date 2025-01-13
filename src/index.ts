116802c56b45607769ca126fbb9a674ac512408d
Bonekazz <hierrofernandes23@gmail.com>
2025-01-13 16:29:33 -0300

    removed unnecessary api call

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import prisma from "../prisma/prisma";
 import { Prisma } from "@prisma/client";
 import cron from "node-cron";
 import { scrapeReviews } from "./modules/scraper";
-import requiredGlobals, { CRONJOB_KEY, ENVIRONMENT, checkMissingRequiredGlobals } from "./modules/config/globals";
+import requiredGlobals, { ENVIRONMENT, checkMissingRequiredGlobals } from "./modules/config/globals";
 import { fireEmailAlert } from "./modules/email";
 
 dotenv.config();
@@ -41,34 +41,6 @@ function isPrismaError(error: any) {
     error instanceof Prisma.PrismaClientInitializationError;
 }
 
-async function hitDavidsEndpoint() {
-
-  try {
-    const endpoint = "https://davidsgarage.pro/api";
-    console.log("> Calling extra endpoint: " + endpoint);
-    const req = await fetch(endpoint, {
-      method: "POST", 
-      headers: {
-        "Authorization": `Bearer ${CRONJOB_KEY}`
-      },
-      body: JSON.stringify({message: "sup bro!"})
-    });
-
-    const body = await req.text() ? await req.json() : undefined;
-
-    if (!req.ok) {
-      console.error(`> [Error] Request to '${endpoint}' return bad status code: ${req.status}. body: ${body}\n`);
-      return await fireEmailAlert(`Request to '${endpoint}' return bad status code: ${req.status}. body: ${body}`);
-    }
-
-    console.log(`[Success] Request successfully made.\n`);
-    
-  } catch (error) {
-    console.error(`> [Error] Error. Details:\n${error}\n`);
-    await fireEmailAlert(error);
-  }
-}
-
 async function main() {
 
   try {
@@ -104,7 +76,5 @@ async function main() {
 
     console.error(`> [Error] Unhandled Error. Details:\n${error}\n`);
     await fireEmailAlert(error.message);
-  } finally {
-    await hitDavidsEndpoint();
-  }
+  } 
 }
